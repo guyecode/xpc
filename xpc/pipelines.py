26595466e6d6e350fe25e14aa67b417e71abf6ea
guye <guye999@gmail.com>
2018-05-14 23:59:05 +0800

    fix insert sql on duplicate

--- xpc/pipelines.py
+++ xpc/pipelines.py
@@ -4,7 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
 import pymysql
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class MysqlPipeline(object):
@@ -14,30 +19,33 @@ class MysqlPipeline(object):
         self.cur = None
 
     def open_spider(self, spider):
-        # 初始化mysql连接
         self.conn = pymysql.connect(
             host='127.0.0.1',
             port=3306,
             user='root',
             password='',
-            db='xpc2',
-            charset='utf8'
+            db='xpc1704',
+            charset='utf8mb4'
         )
-        # 初始化游标对象
         self.cur = self.conn.cursor()
 
     def process_item(self, item, spider):
         if not hasattr(item, 'table_name'):
             return item
         cols, values = zip(*item.items())
-        sql = "INSERT INTO `%s` (%s) VALUES (%s)" % \
-              (item.table_name, ','.join(cols), ','.join(['%s'] * len(values)))
-        self.cur.execute(sql, values)
+        sql = "INSERT INTO `{}` ({}) VALUES ({}) " \
+              "ON DUPLICATE KEY UPDATE {}".format(
+                  item.table_name,
+                  ','.join(cols),
+                  ','.join(['%s'] * len(values)),
+                  ','.join(['{}=%s'.format(k) for k in cols])
+               )
+        logger.debug(sql)
+        self.cur.execute(sql, values * 2)
+        logger.debug(self.cur._last_executed)
         self.conn.commit()
-        print(self.cur._last_executed)
         return item
 
     def close_spider(self, spider):
-        """关闭mysql连接和游标"""
         self.cur.close()
         self.conn.close()
