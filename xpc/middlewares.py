21d5da943eb1bb62a1ce0eeed62f97043ddbffcf
guye <guye999@gmail.com>
2018-06-17 12:02:35 +0800

    add comment

--- xpc/middlewares.py
+++ xpc/middlewares.py
@@ -6,27 +6,36 @@
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 import random
 from scrapy.exceptions import NotConfigured
-from scrapy.downloadermiddlewares.httpproxy import HttpProxyMiddleware
 
 
 class RandomProxyMiddleware(object):
+    """随机动态IP代理池"""
 
     def __init__(self, settings):
+        # 2. 判断是否配置了IP代理池
+        if not settings.getlist('PROXIES'):
+            raise NotConfigured
+        # 从配置里读取出来
         self.proxies = settings.getlist('PROXIES')
+        # 设置代理IP的错误统计，默认设置为0
         self.stats = {}.fromkeys(self.proxies, 0)
-        self.max_failed = 1
+        # 最大失败次数
+        self.max_failed = 3
 
     @classmethod
     def from_crawler(cls, crawler):
+        # 1. 首先判断是否启用了proxy
         if not crawler.settings.getbool('HTTPPROXY_ENABLED'):
             raise NotConfigured
-        if not crawler.settings.getlist('PROXIES'):
-            raise NotConfigured
+        # 创建中间件对象
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
-        request.meta['proxy'] = random.choice(self.proxies)
-        print('use %s as proxy' % request.meta['proxy'])
+        # 3. 随机设置一个代理
+        # 通过设置meta内的proxy属性，利用系统本身的proxy中间件去实现代理
+        if 'proxy' not in request.meta:
+            proxy_url = random.choice(self.proxies)
+            request.meta['proxy'] = proxy_url
 
     def remove_proxy(self, proxy):
         if proxy in self.proxies:
@@ -34,25 +43,29 @@ class RandomProxyMiddleware(object):
             print('proxy %s removed from proxies list' % proxy)
 
     def process_response(self, request, response, spider):
+        # 4. 代理正常，但对方服务器返回了错误的状态码，有可能是被封掉
         cur_proxy = request.meta['proxy']
+        if response.status != 200:
+            print('none 200 status code: %s when use %s'
+                  % (response.status, cur_proxy))
         if response.status >= 400:
             self.stats[cur_proxy] += 1
+        # 如果异常status在该代理上出现了N次，也从代理池中删除
         if self.stats[cur_proxy] >= self.max_failed:
             self.remove_proxy(cur_proxy)
+            # 删除当前request对象的代理，并返回重新调度下载
+            del request.meta['proxy']
+            return request
+        # 如果一切正常，返回response对象，以让下面的中间件继续执行。
         return response
 
     def process_exception(self, request, exception, spider):
-        print('raise exception:%s when use %s.' % (exception, request.meta['proxy']))
-        self.remove_proxy(request.meta['proxy'])
-
-
-
-
-
-
-
-
-
-
-
-
+        # 4. 如果代理不可用，则会触发此方法
+        cur_proxy = request.meta['proxy']
+        # print一下异常信息
+        print('raise exception: %s when use %s' % (exception, cur_proxy))
+        # 从代理池中删除该代理
+        self.remove_proxy(cur_proxy)
+        # 删除当前request对象的代理，并返回重新调度下载
+        del request.meta['proxy']
+        return request
--- xpc/spiders/discovery.py
+++ xpc/spiders/discovery.py
@@ -2,147 +2,185 @@
 import json
 import scrapy
 from scrapy import Request
-from xpc.items import PostItem, ComposerItem, CommentItem, CopyrightItem
-from scrapy.exceptions import CloseSpider
-from xpc.utils import convert_int as ci, strip
+from scrapy_redis.spiders import RedisSpider
+from xpc.items import PostItem, CommentItem, ComposerItem, CopyrightItem
 
-vip_map = {
-        'yellow-v': 1,
-        'blue-v': 2,
-    }
+def strip(s):
+    """去掉字符串中特殊符号"""
+    if s:
+        return s.strip().replace('&nbsp;', '').replace('\xa0', '')
+    return ''
 
 
-class PostsSpider(scrapy.Spider):
-    name = 'posts'
-    allowed_domains = ['www.xinpianchang.com']
-    # 热门
-    start_urls = ['http://www.xinpianchang.com/channel/index/sort-like']
-    # 最新
-    # start_urls = ['http://www.xinpianchang.com/channel/index/id-0/sort-addtime/type-0']
-    custom_settings = {
-        'ITEM_PIPELINES': {
-            'xpc.pipelines.PostPipeline': 300,
-            'xpc.pipelines.ComposerPipeline': 301,
-            'xpc.pipelines.CommentPipeline': 302,
-            'xpc.pipelines.CopyrightItemPipeline': 303,
-        }
-    }
+def convert_int(s):
+    """去掉数字字符串中的逗号，并将其转换成int类型"""
+    if not s:
+        return 0
+    return int(s.replace(',', ''))
+ci = convert_int
+
+
+class DiscoverySpider(RedisSpider):
+    name = 'discovery'
+    allowed_domains = ['xinpianchang.com']
+    # start_urls = ['http://www.xinpianchang.com/channel/index/sort-like']
+    # start_urls = ['http://www.xinpianchang.com/channel/index/type-0/sort-like/duration_type-0/resolution_type-/page-20']
+
+    def start_requests(self):
+        """重写默认的start_requests方法，以yield自定义的request对象"""
+        for url in self.start_urls:
+            yield Request(url, dont_filter=True, meta={'dont_merge_cookies': True})
+
+    def make_requests_from_url(self, url):
+        """ This method is deprecated. """
+        return Request(url, dont_filter=True, meta={'dont_merge_cookies': True})
 
     def parse(self, response):
-        self.logger.info('%s %s %s ' % (response.status, len(response.text), response.url))
-        post_url = 'http://www.xinpianchang.com/a%s?from=channel'
-        post_list = response.xpath('//ul[@class="video-list"]/li')
-        # post_list = response.xpath('//ul[@class="video-list"]/li/@data-articleid').extract()
-        for post in post_list:
-            post_id = post.xpath('./@data-articleid').extract_first()
-            request = Request(post_url % post_id, callback=self.parse_post)
-            request.meta['pid'] = post_id
-            request.meta['thumbnail'] = post.xpath('./a/img/@_src').extract_first()
-            request.meta['duration'] = post.xpath('.//span[contains(@class, "duration")]/text()').get()
+        url = 'http://www.xinpianchang.com/a%s?from=ArticleList'
+        # 取出列表页所有的视频节点
+        posts = response.xpath('//ul[@class="video-list"]/li')
+        # 循环遍历所有的节点，每一个节点对应一个视频
+        for post in posts:
+            # 取出视频的ID
+            pid = post.xpath('./@data-articleid').extract_first()
+            # 根据ID拼接出视频详情页的url
+            request = Request(url % pid, callback=self.parse_post)
+            # 利用request.meta属性的作用，将pid传递给回调函数
+            request.meta['pid'] = pid
+            # 将列表页的缩略图传递给回调函数
+            request.meta['thumbnail'] = post.xpath('./a/img/@_src').get()
+            # 取一下播放时长
+            duration = post.xpath('.//span[contains(@class, "duration")]/text()').get()
+            # 把页面的显示时长（02' 46''）转换成int秒数
+            minutes, seconds, *_ = duration.splti("'")
+            request.meta['duration'] = ci(minutes) * 60 + ci(seconds)
             yield request
-
-        next_page = response.xpath('//div[@class="page"]/a[last()]/@href').get()
-        self.logger.info('next_page: %s' % next_page)
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
+        # 获取底部翻页区内的所有链接，scrapy会自动过滤掉重复链接
+        next_pages = response.xpath('//div[@class="page"]/a/@href').extract()
+        for next_page in next_pages:
+            # 给request.meta里面加上dont_merge_cookies属性
+            # 以告诉scrapy框架强制使用我们自己设定的cookies
+            yield Request(next_page,
+                          callback=self.parse,
+                          meta={'dont_merge_cookies': True})
 
     def parse_post(self, response):
-        # 提取作品信息
+        # 创建一个Item对象
         post = PostItem()
+        # 取出上一步函数中传递过来的值
         post['pid'] = response.meta['pid']
-        post['title'] = response.xpath('//div[@class="title-wrap"]/h3/text()').extract_first()
         post['thumbnail'] = response.meta['thumbnail']
-        post['preview'] = strip(response.xpath('//div[@class="filmplay"]//img/@src').extract_first())
-        video = response.xpath('//video[@id="xpc_video"]/@src') or response.xpath('//div[@class="td-player"]//video/@src')
-        post['video'] = video.extract_first()
-        post['video_format'] = strip(response.xpath('//span[contains(@class, "video-format")]/text()').extract_first())
-        duration = response.meta['duration']
-        if duration:
-            # 将播放时长由文本格式（比如：19:00）转换为int秒
-            duration = [int(i) for i in duration.replace("'", "").split(' ')]
-            post['duration'] = duration[0] * 60 + duration[1]
-        post['category'] = response.xpath('//span[@class="cate v-center"]/text()').extract_first()
-        post['created_at'] = response.xpath('//span[contains(@class,"update-time")]/i/text()').extract_first()
-        post['play_counts'] = ci(response.xpath('//i[contains(@class,"play-counts")]/@data-curplaycounts').extract_first())
-        post['like_counts'] = ci(response.xpath('//i[contains(@class,"like-counts")]/@data-counts').extract_first())
-        post['description'] = strip(response.xpath('//p[contains(@class,"desc")]/text()').extract_first(default=''))
+        # 提取出预览图
+        post['preview'] = response.xpath(
+            '//div[@class="filmplay"]//img/@src').get()
+        # 视频URL
+        post['video'] = response.xpath(
+            '//a[@id="player"]/@href').get()
+        # 视频播放时长
+        post['duration'] = response.meta['duration']
+        # 视频标题
+        post['title'] = response.xpath(
+            '//div[@class="filmplay-info"]//h3/text()').get()
+        # 视频分类，由于取出的是多个，需要先strip，再join
+        category = response.xpath(
+            '//span[contains(@class, "cate")]//text()').extract()
+        post['category'] = ''.join([strip(c) for c in category]).strip()
+        # 创建时间
+        post['created_at'] = response.xpath(
+            '//span[contains(@class, "update-time")]/i/text()').get()
+        # 播放次数
+        post['play_counts'] = ci(response.xpath(
+            '//i[contains(@class, "play-counts")]/@data-curplaycounts').get())
+        # 该视频被点赞次数
+        post['like_counts'] = ci(response.xpath(
+            '//span[contains(@class, "like-counts")]/@data-counts').get())
+        # 描述
+        post['description'] = strip(response.xpath(
+            '//p[contains(@class, "desc")]/text()').get())
         yield post
 
-        # 抓取评论
-        comment_api = 'http://www.xinpianchang.com/article/filmplay/ts-getCommentApi/id-%s/page-1'
-        request = Request(comment_api % (post['pid']), callback=self.parse_comment)
-        yield request
-
-       
-        composer_url = 'http://www.xinpianchang.com/u%s'
-        composers = []
-        for elem in response.xpath('//div[@class="user-team"]//ul[@class="creator-list"]/li'):
-            cid = elem.xpath('.//a[@class="head-wrap"]/@data-userid').extract_first()
-            # 抓取作者信息
-            request = Request(composer_url % cid, callback=self.parse_composer)
+        # 作者的url模板
+        url = 'http://www.xinpianchang.com/u%s?from=articleList'
+        # 取出所有的作者节点
+        composers = response.xpath('//div[@class="user-team"]//ul[@class="creator-list"]/li')
+        for composer in composers:
+            # 取出作者的ID
+            cid = composer.xpath('./a/@data-userid').get()
+            # 提取作者与视频的对应关系
+            copyright = CopyrightItem()
+            copyright['cid'] = cid
+            copyright['pid'] = post['pid']
+            # 联合主键，保证Item的唯一性
+            copyright['pcid'] = '%s_%s' % (cid, post['pid'])
+            copyright['roles'] = composer.xpath(
+                './/span[contains(@class, "roles")]/text()').get() or ''
+            yield copyright
+            # 拼装成作者的主页URL，并创建request对象
+            request = Request(url % cid, callback=self.parse_composer)
+            # 把cid传递给回调函数
             request.meta['cid'] = cid
             yield request
 
-            # 提取著作权信息
-            cr = CopyrightItem()
-            cr['pid'] = post['pid']
-            cr['cid'] = cid
-            cr['pcid'] = '%s_%s' % (cr['pid'], cid)
-            cr['roles'] = elem.xpath('.//span[contains(@class, "roles")]/text()').extract_first()
-            yield cr
-
-    def parse_comment(self, response):
-        result = json.loads(response.text)
-        comments = result['data']['list']
-        for c in comments:
-            comment = CommentItem()
-            comment['commentid'] = c['commentid']
-            comment['pid'] = c['articleid'] or 0
-            comment['cid'] = c['userInfo']['userid'] or 0
-            comment['avatar'] = c['userInfo']['face']
-            comment['uname'] = strip(c['userInfo']['username'])
-            comment['created_at'] = c['addtime']
-            comment['content'] = c['content']
-            comment['like_counts'] = c['count_approve']
-            if c['reply']:
-                comment['reply'] = c['reply']['commentid'] or 0
-            yield comment
-
-        # 是否还有更多评论，有则继续抓取
-        next_page = result['data']['next_page_url']
-        if next_page:
-            yield response.follow(next_page)
+        # 抓取异步加载的评论信息，根据ajax参数可以指定返回html还是json
+        comment_url = 'http://www.xinpianchang.com/article/filmplay/ts-getCommentApi?id=%s&ajax=0&page=1'
+        request = Request(comment_url % post['pid'], callback=self.parse_comment)
+        yield request
 
     def parse_composer(self, response):
         composer = ComposerItem()
         composer['cid'] = response.meta['cid']
-        composer['name'] = response.xpath('//p[contains(@class,"creator-name")]/text()').extract_first()
-        composer['banner'] = response.xpath('//div[@class="banner-wrap"]/@style').extract_first()[21:-1]
-        elem = response.xpath('//span[@class="avator-wrap-s"]')
-        composer['avatar'] = elem.xpath('./img/@src').extract_first()
-        auth_style = elem.xpath('./span/@class').get()
-        if auth_style:
-            composer['verified'] = vip_map.get(auth_style.split(' ')[-1], 0)
-        composer['intro'] = strip(response.xpath('//p[contains(@class,"creator-desc")]/text()').extract_first())
-        composer['like_counts'] = ci(response.xpath('//span[contains(@class,"like-counts")]/text()').extract_first())
-        composer['fans_counts'] = ci(response.xpath('//span[contains(@class,"fans-counts")]/text()').extract_first())
-        composer['follow_counts'] = ci(response.xpath('//span[@class="follow-wrap"]/span[2]/text()').extract_first())
-        composer['location'] = response.xpath('//span[contains(@class, "icon-location")]/following-sibling::span[1]/text()').get()
-        composer['career'] = response.xpath('//span[contains(@class, "icon-career")]/following-sibling::span[1]/text()').get()
+        # 取一下用户主责的背景大图
+        background = response.xpath('//div[@class="banner-wrap"]/@style').get()
+        # 因为大图是写在css样式里的，所以用切片操作提出出来
+        composer['banner'] = background[21:-1]
+        # 用户头像
+        composer['avatar'] = response.xpath(
+            '//span[@class="avator-wrap-s"]/img/@src').get()
+        # 用户名称
+        composer['name'] = response.xpath(
+            '//p[contains(@class, "creator-name")]/text()').get()
+        # 自我介绍
+        composer['intro'] = response.xpath(
+            '//p[contains(@class, "creator-desc")]/text()').get()
+        # 用户人气，也就是被点赞的次数总和
+        composer['like_counts'] = ci(response.xpath(
+            '//span[contains(@class, "like-counts")]/text()').get())
+        # 关注他的用户的数量
+        composer['fans_counts'] = ci(response.xpath(
+            '//span[contains(@class, "fans-counts")]/text()').get())
+        # 他关注其他用户的数量
+        composer['follow_counts'] = ci(response.xpath(
+            '//span[@class="follow-wrap"]/span[2]/text()').get())
+        # 所在位置
+        composer['location'] = strip(response.xpath(
+            '//span[contains(@class, "icon-location")]/'
+            'following-sibling::span[1]/text()').get())
+        # 职业
+        composer['career'] = response.xpath(
+            '//span[contains(@class, "icon-career")]/'
+            'following-sibling::span[1]/text()').get()
         yield composer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def parse_comment(self, response):
+        # 直接加载json结果
+        result = json.loads(response.text)
+        comments = result['data']['list']
+        for c in comments:
+            comment = CommentItem()
+            comment['pid'] = c['articleid']
+            comment['cid'] = c['userInfo']['userid']
+            comment['uname'] = c['userInfo']['username']
+            comment['avatar'] = c['userInfo']['face']
+            comment['commentid'] = c['commentid']
+            comment['content'] = c['content']
+            comment['created_at'] = c['addtime']
+            comment['like_counts'] = c['count_approve']
+            # 判断本条评论是否是回复其他的评论
+            if c['reply']:
+                # 如果是，把回复的那条评论的ID保存下来
+                comment['reply'] = c['reply']['commentid'] or 0
+            yield comment
+        # 判断评论是否有下一页
+        next_page = result['data']['next_page_url']
+        if next_page:
+             yield Request(next_page, callback=self.parse_comment)
